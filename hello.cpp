1f909364fa6797d0a5cff46c5cdf552c645e1b0e
Pejman Habashi <Pejman.Habashi@radixinc.ca>
2019-12-06 14:03:38 +0000

    Initial commit

---
+++ hello.cpp
@@ -0,0 +1,9 @@
+#include <hello.h>
+
+#include <iostream>
+
+void hello( std::string name ) {
+	using namespace std;
+	cout << "Hello " << name << endl;
+}
+
---
+++ hello.h
@@ -0,0 +1,7 @@
+#ifndef __hello__
+#define __hello__
+#include <string>
+
+extern void hello ( std::string name );
+
+#endif
